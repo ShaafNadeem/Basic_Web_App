2fc6f152515c037c356cd4eeea6d536112877432
shaaf <shaaf.nadeem2001@gmail.com>
2023-08-01 18:40:31 +0500

    Initial Commit

---
+++ app1.py
@@ -0,0 +1,49 @@
+while True:
+    user_action = input('Type add, show, edit, complete or exit:')
+    user_action = user_action.strip()
+    match user_action:
+        case 'add':
+            todo = input('Enter a todo:')+'\n'
+            with open ('text.txt','r') as file:
+                todos = file.readlines()
+
+            todos.append(todo)
+
+            with open ('text.txt','w') as file:
+                todos = file.writelines(todos)
+        case 'show':
+            with open ('text.txt','r') as file:
+                todos = file.readlines()
+            for index,item in enumerate(todos):
+                item = item.strip('\n')
+                row = f'{index+1}-{item}'
+                print(row)
+        case 'edit':
+            number = int(input('Number of the todo to ed0it:0'))
+            index = number -1
+            with open ('text.txt','r') as file:
+                todos = file.readlines()
+            
+            new_todo = input('Enter new todo:')
+            todos[index] = new_todo + '\n'
+            
+            with open ('text.txt','w') as file:
+                todos = file.writelines(todos)
+        case 'complete':
+            number = int(input('Number of the todo to complete:'))
+            index = number -1
+
+            with open ('text.txt','r') as file:
+                todos = file.readlines()
+            
+            todo_to_remove = todos[index].strip('\n') 
+            todos.pop(index)
+
+            with open ('text.txt','w') as file:
+                todos = file.writelines(todos)
+
+            message = f'{todo_to_remove} is removed from your list.'
+            print(message)
+        case 'exit':
+            break
+print('Bye!')
